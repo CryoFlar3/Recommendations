681119489734947fb253f2041f3783355ec96fe6
Nyjel <nyjel.dukes@gmail.com>
2020-01-22 17:55:54 -0500

    Fixed some errors

--- app/src/main/java/org/computermentors/sample/googleplayservices/api/Api.java
+++ app/src/main/java/org/computermentors/sample/googleplayservices/api/Api.java
@@ -2,9 +2,9 @@ package org.computermentors.sample.googleplayservices.api;
 
 import org.computermentors.sample.googleplayservices.model.ActiveListings;
 
-import retrofit2.Callback;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
 
 public interface Api {
 
