f96debd939053e95b02de415cfb2763fd3c02a59
N. Dukes <nyjel.dukes@gmail.com>
2020-01-25 23:02:11 -0500

    Updated RecyclerView to current version and added UI elements

--- app/src/main/java/org/computermentors/sample/googleplayservices/MainActivity.java
+++ app/src/main/java/org/computermentors/sample/googleplayservices/MainActivity.java
@@ -1,14 +1,45 @@
 package org.computermentors.sample.googleplayservices;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private View progressBar;
+    private TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView =  findViewById(R.id.recyclerview);
+        progressBar =  findViewById(R.id.progressbar);
+        errorView =  findViewById(R.id.errorview);
+
+        showLoading();
+    }
+
+    public void showLoading(){
+        progressBar.setVisibility(View.VISIBLE);
+        errorView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+    }
+
+    public void showList(){
+        progressBar.setVisibility(View.GONE);
+        errorView.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+    }
+
+    public void showError(){
+        progressBar.setVisibility(View.GONE);
+        errorView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
     }
 }
